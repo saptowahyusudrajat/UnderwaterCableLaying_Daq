2e66af9b0466c9c0791494af58e24d34d38c7461
Sapto Wahyu Sudrajat <129891157+saptowahyusudrajat@users.noreply.github.com>
2024-09-09 06:10:31 +0700

    Sync files to Git

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace nyoba_browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(441, 244);
+            this.chromiumWebBrowser1.TabIndex = 0;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(494, 150);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Minimum = -360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(283, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(501, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ROLL :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(542, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(542, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(501, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "PITCH :";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(494, 222);
+            this.trackBar2.Maximum = 360;
+            this.trackBar2.Minimum = -360;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(283, 45);
+            this.trackBar2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace nyoba_browser
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Start the browser after initialize global component
+            InitializeChromium();
+            // Register an object in javascript named "cefCustomObject" with function of the CefCustomObject class :3
+           //chromiumWebBrowser1.RegisterJsObject("cefCustomObject", new CefCustomObject(chromiumWebBrowser1, this));
+
+
+            
+
+
+        }
+
+        public static int yaw = 0;
+        public static int pitch = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+
+            // Note that if you get an error or a white screen, you may be doing something wrong !
+            // Try to load a local file that you're sure that exists and give the complete path instead to test
+            // for example, replace page with a direct path instead :
+            // String page = @"C:\Users\SDkCarlos\Desktop\afolder\index.html";
+
+            String page = string.Format(@"{0}\sudah benar load.html", Application.StartupPath);
+            //String page = @"C:\Users\SDkCarlos\Desktop\artyom-HOMEPAGE\index.html";
+
+            if (!File.Exists(page))
+            {
+                MessageBox.Show("Error The html file doesn't exists : " + page);
+            }
+
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromiumWebBrowser1.Load(page);
+
+            // Add it to the form and fill it to the form window.
+           // this.Controls.Add(chromiumWebBrowser1);
+           // chromeBrowser.Dock = DockStyle.Fill;
+
+            // Allow the use of local resources in the browser
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chromiumWebBrowser1.BrowserSettings = browserSettings;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+            
+        }
+
+
+
+
+
+        private async void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            yaw = trackBar1.Value;
+            label2.Text = yaw.ToString();
+            pitch = trackBar2.Value;
+            label3.Text = pitch.ToString();
+            string scrip = "scene.registerBeforeRender(function(){dude.rotation= new BABYLON.Vector3(("+yaw.ToString()+"*Math.PI/180), (0*Math.PI/180), ("+pitch.ToString()+"*Math.PI/180));});";
+            //MessageBox.Show(scrip);
+            
+
+
+            //Check if the browser can execute JavaScript and the ScriptTextBox is filled
+            if (chromiumWebBrowser1.CanExecuteJavascriptInMainFrame)
+            {
+                //Evaluate javascript and remember the evaluation result
+                JavascriptResponse response = await chromiumWebBrowser1.EvaluateScriptAsync(scrip);
+
+                if (response.Result != null)
+                {
+                    //Display the evaluation result if it is not empty
+                    MessageBox.Show(response.Result.ToString(), "JavaScript Result");
+                }
+            }
+
+
+
+        }
+
+
+
+        
+
+       
+
+
+
+
+    }
+}
